72d06934235f664f0de05232b73228e4677a08d6
Mads T. Kristiansen <madstk1@gmail.com>
2019-09-26 14:42:50 +0200

    Added custom return codes.

--- src/headers/sck_log.h
+++ src/headers/sck_log.h
@@ -1,6 +1,13 @@
 #ifndef SCK_LOG_H
 #define SCK_LOG_H
 
+#define SCK_OK                          0
+#define SCK_ERROR                      -1
+#define SCK_INVALID_PARAMETERS         -2
+#define SCK_CALL_DECLINED              -3
+#define SCK_CALL_ABORTED               -4
+#define SCK_MEMORY_ERROR               -5
+
 #define SCK_LOG_LEVEL_DEBUG             0b0001 /* 0001 */
 #define SCK_LOG_LEVEL_WARNING           0b0010 /* 0010 */
 #define SCK_LOG_LEVEL_ERROR             0b0100 /* 0100 */
