7fad201d61359a6e96d17b6321556e3d02249eb5
Mads T. Kristiansen <madstk1@gmail.com>
2019-09-26 15:21:57 +0200

    Fixed log buffering issues.

--- src/core/sck_log.c
+++ src/core/sck_log.c
@@ -6,20 +6,14 @@
 /* LOG DEBUG */
 int sck_log_debug(const char * fmt, ...) {
     if((SCK_LOG_LEVEL & SCK_LOG_LEVEL_DEBUG)) {
-        char *buffer = malloc(sizeof(char) * 0);
-        int len;
+        char *buffer = malloc(sizeof(char) * 256);
         va_list vl;
         
         va_start(vl, fmt);
-        len = vsnprintf( buffer, sizeof(buffer), fmt, vl);
+        vsprintf(buffer, fmt, vl);
         va_end(vl);
 
-        buffer = realloc(buffer, sizeof(char) * len);
-        va_start(vl, fmt);
-        vsnprintf( buffer, sizeof(buffer), fmt, vl);
-        va_end(vl);
-
-        printf("[DEBUG] %s", buffer);
+        printf("[DEBUG] %s\n", buffer);
         return 0;
     }
     return 1;
@@ -28,20 +22,14 @@ int sck_log_debug(const char * fmt, ...) {
 /* LOG WARNING */
 int sck_log_warning(const char * fmt, ...) {
     if((SCK_LOG_LEVEL & SCK_LOG_LEVEL_WARNING)) {
-        char *buffer = malloc(sizeof(char) * 0);
-        int len;
+        char *buffer = malloc(sizeof(char) * 256);
         va_list vl;
         
         va_start(vl, fmt);
-        len = vsnprintf( buffer, sizeof(buffer), fmt, vl);
+        vsprintf(buffer, fmt, vl);
         va_end(vl);
 
-        buffer = realloc(buffer, sizeof(char) * len);
-        va_start(vl, fmt);
-        vsnprintf( buffer, sizeof(buffer), fmt, vl);
-        va_end(vl);
-
-        printf("[WARNING] %s", buffer);
+        printf("[WARNING] %s\n", buffer);
         return 0;
     }
     return 1;
@@ -50,17 +38,11 @@ int sck_log_warning(const char * fmt, ...) {
 /* LOG ERROR */
 int sck_log_error(const char * fmt, ...) {
     if((SCK_LOG_LEVEL & SCK_LOG_LEVEL_ERROR)) {
-        char *buffer = malloc(sizeof(char) * 0);
-        int len;
+        char *buffer = malloc(sizeof(char) * 256);
         va_list vl;
         
         va_start(vl, fmt);
-        len = vsnprintf( buffer, sizeof(buffer), fmt, vl);
-        va_end(vl);
-
-        buffer = realloc(buffer, sizeof(char) * len);
-        va_start(vl, fmt);
-        vsnprintf( buffer, sizeof(buffer), fmt, vl);
+        vsprintf(buffer, fmt, vl);
         va_end(vl);
 
         printf("[ERROR] %s", buffer);
@@ -72,17 +54,11 @@ int sck_log_error(const char * fmt, ...) {
 /* LOG CRITICAL */
 int sck_log_critical(const char * fmt, ...) {
     if((SCK_LOG_LEVEL & SCK_LOG_LEVEL_CRITICAL)) {
-        char *buffer = malloc(sizeof(char) * 0);
-        int len;
+        char *buffer = malloc(sizeof(char) * 256);
         va_list vl;
         
         va_start(vl, fmt);
-        len = vsnprintf( buffer, sizeof(buffer), fmt, vl);
-        va_end(vl);
-
-        buffer = realloc(buffer, sizeof(char) * len);
-        va_start(vl, fmt);
-        vsnprintf( buffer, sizeof(buffer), fmt, vl);
+        vsprintf(buffer, fmt, vl);
         va_end(vl);
 
         printf("[CRITICAL] %s", buffer);
