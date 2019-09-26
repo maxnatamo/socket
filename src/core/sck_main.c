5cee366a71955a8b43b7ab90345b6bebe966fd48
Mads T. Kristiansen <madstk1@gmail.com>
2019-09-26 15:23:51 +0200

    Added further error-handling for sck_main.c

--- src/core/sck_main.c
+++ src/core/sck_main.c
@@ -29,26 +29,28 @@ int main(int argc, char *argv[])
         sck_http_request_t *request   = malloc(sizeof(sck_http_request_t));
         sck_http_response_t *response = malloc(sizeof(sck_http_response_t));
 
-        sck_socket_accept(socket, request);
-        sck_http_handle_request(request);
+        err = sck_socket_accept(socket, request);
+        if(err != SCK_OK) {
+            continue;
+        }
 
-        response->conn         = socket;
-        response->httpmajor    = 1;
-        response->httpminor    = 0;
-        response->statuscode   = 200;
-        response->contenttype  = "text/html";
-        response->content      = "<html><body><h1>lol</h1></body></html>";
+        err = sck_http_handle_request(request);
+        if(err != SCK_OK) {
+            continue;
+        }
+
+        response->conn          = socket;
         response->contentlength = strlen(response->content);
 
         // Send HTTP response
-        sck_http_write (request, response);
+        err = sck_http_write (request, response);
         if(err != SCK_OK) {
-            return err;
+            continue;
         }
 
-        sck_http_close (request);
+        err = sck_http_close (request);
         if(err != SCK_OK) {
-            return err;
+            continue;
         }
     }
     return 0;
